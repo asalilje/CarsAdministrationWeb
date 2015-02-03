115db4a949643b4a434faa0fc38b0b2cb2268868
Åsa Liljegren <asa.lilje@gmail.com>
2015-02-03 23:04:18 +0100

    Extended jsonresult and added helpers

--- src/Cars.Administration.Web/Infrastructure/CarAdministrationController.cs
+++ src/Cars.Administration.Web/Infrastructure/CarAdministrationController.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
+using Cars.Administration.Web.ActionResults;
 using Cars.Administration.Web.Filters;
 using Microsoft.Web.Mvc;
 
 namespace Cars.Administration.Web.Infrastructure
 {
-    [CurrencySelectListPopulator]
-    public abstract class CarAdministrationController : Controller
-    {
-        protected ActionResult RedirectToAction<TController>(
-            Expression<Action<TController>> action) where TController : Controller
-        {
-            return ControllerExtensions.RedirectToAction(this, action);
-        }
-    }
+		[CurrencySelectListPopulator]
+		public abstract class CarAdministrationController : Controller
+		{
+				protected ActionResult RedirectToAction<TController>(
+						Expression<Action<TController>> action) where TController : Controller
+				{
+						return ControllerExtensions.RedirectToAction(this, action);
+				}
+
+			protected StandardJsonResult JsonValidationError()
+			{
+				var result = new StandardJsonResult();
+				foreach (var validationError in ModelState.Values.SelectMany(v => v.Errors))
+				{
+					result.AddError(validationError.ErrorMessage);
+				}
+				return result;
+			}
+
+			protected StandardJsonResult JsonError(string errorMessage)
+			{
+				var result = new StandardJsonResult();
+				result.AddError(errorMessage);
+				return result;
+			}
+
+			protected StandardJsonResult<T> JsonSuccess<T>(T data)
+			{
+				return new StandardJsonResult<T> {Data = data};
+			}
+		}
 }
